b23048f8571154de3f8b9fb9f03aa7596293c772
MarinaTalochka <153221203+MarinaTalochka@users.noreply.github.com>
2024-01-05 18:54:24 +0300

    добавление кода д.з.4

--- script.js
+++ script.js
@@ -7,3 +7,19 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector('#input');
+const list = document.querySelector('#items');
+
+input.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter' && input.value !== '') {
+    const newItem = document.createElement('div');
+    
+           newItem.textContent = input.value;
+    list.appendChild(newItem);
+    input.value = '';
+  }
+})
+list.addEventListener('click',function() {
+    list.classList.toggle('done')
+  });
---
+++ ысю.js
@@ -0,0 +1,6 @@
+const sendInput = document.querySelector('#input');
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter')
+    console.log (event)
+} )
+const container = document.querySelector('.container');
